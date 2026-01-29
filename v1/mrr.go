d8e27843da17fc538775c0819ba645b7fe249642
almeida <hello@eualmeida.com.br>
2026-01-29 15:07:39 -0300

    feat(APIRevenueByPeriod): add missing struct in v1/mrr

--- v1/mrr.go
+++ v1/mrr.go
@@ -12,3 +12,13 @@ type APIMRRInfo struct {
 	MRR                      uint64 `json:"mrr"`
 	TotalActiveSubscriptions uint64 `json:"totalActiveSubscriptions"`
 }
+
+// https://docs.abacatepay.com/pages/trustMRR/reference#receita-por-per%C3%ADodo
+type APIRevenueByPeriod struct {
+	TotalRevenue       uint64 `json:"totalRevenue"`
+	TotalTransactions  uint64 `json:"totalTransactions"`
+	TransactionsPerDay map[string]struct {
+		Amount uint64 `json:"amount"`
+		Count  uint64 `json:"count"`
+	} `json:"transactionsPerDay"`
+}
